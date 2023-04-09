16c8327d16b5ea1ebaa90b06ff2d02656c27abd2
Karen Vicente <karenyasmin37@gmail.com>
2023-04-08 21:23:21 -0300

    initial commit

--- soundUp.ps1
+++
@@ -1,7 +0,0 @@
-
-#Dowload link: https://www.nirsoft.net/utils/nircmd.html
-$nircmdPath = "C:\nircmd-x64\nircmd.exe"
-
-# Diminua o volume do sistema em 20 unidades
-& $nircmdPath mutesysvolume 0
-& $nircmdPath changesysvolume +10000
